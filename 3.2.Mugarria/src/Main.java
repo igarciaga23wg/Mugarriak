5e8e3103e05d62381138e1191366056ece6aa25c
Igor Garcia <igarciaga23wg@ikzubirimanteo.com>
2024-03-21 11:37:38 +0100

    ComboBox egiten

--- 3.2.Mugarria/src/Main.java
+++ 3.2.Mugarria/src/Main.java
@@ -1,7 +1,31 @@
-import java.util.Scanner;
-public class Main {
+import javax.swing.*;
+import java.awt.event.*;
+public class Main extends JFrame{
+        private JComboBox<String> comboBox;
+        public Main()
+        {
+            setLayout(null);
+            comboBox=new JComboBox<String>();
+            comboBox.setBounds(10,10,80,20);
+            add(comboBox);
+            comboBox.addItem("python.txt");
+            comboBox.addItem("c.txt");
+            comboBox.addItem("java.txt");
+            comboBox.addItemListener((ItemListener) this);
+
+        }
+        public void itemStateaChanged(ItemEvent e)
+        {
+            if (e.getSource()==comboBox)
+            {
+                String aukeratuta = (String) comboBox.getSelectedItem();
+                setTitle(aukeratuta);
+            }
+        }
     public static void main(String[] args) {
-        int a=1;
-        System.out.println(a);
+            Main m1 = new Main();
+            m1.setBounds(0,0,200,150);
+            m1.setVisible(true);
+            m1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
