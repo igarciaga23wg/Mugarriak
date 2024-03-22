af7acc6c6bdc67d7d3e0a4a07d5bb468cb8cefa2
Igor Garcia <igordoble10@gmail.com>
2024-03-22 22:45:02 +0100

    Clear botoia eginda

--- 3.2.Mugarria/src/Main.java
+++ 3.2.Mugarria/src/Main.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main extends JFrame implements ItemListener {
     private JComboBox<String> comboBox;
+    private JTextArea textArea;
+
+    private JTextField textfield;
 
     public Main() {
-        setLayout(null);
+        setLayout(new BorderLayout());
         comboBox = new JComboBox<String>();
         comboBox.setBounds(30, 10, 300, 20);
         add(comboBox);
@@ -13,10 +20,33 @@ public class Main extends JFrame implements ItemListener {
         comboBox.addItem("c.txt");
         comboBox.addItem("java.txt");
         comboBox.addItemListener(this);
-    }
+
+
+        JButton garbitubotoia = new JButton("Clear");
+        garbitubotoia.setBounds(350,10,100,20);
+        add(garbitubotoia);
+
+        JButton itxibotoia = new JButton("Close");
+        add(itxibotoia,BorderLayout.SOUTH);
+
+        textfield=new JTextField();
+        textfield.setBounds(600,10,200,300);
+        add(textfield);
+        textArea=new JTextArea();
+        textArea.setBounds(10,50,400,300);
+        add(textArea);
+
+        garbitubotoia.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.setText("");
+            }
+        });
+}
+
 
     @Override
-    public void itemStateChanged(ItemEvent e) { 
+    public void itemStateChanged(ItemEvent e) {
         if (e.getSource() == comboBox) {
             String aukeratuta = (String) comboBox.getSelectedItem();
             setTitle(aukeratuta);
@@ -25,7 +55,7 @@ public class Main extends JFrame implements ItemListener {
 
     public static void main(String[] args) {
         Main m1 = new Main();
-        m1.setBounds(0, 0, 200, 150);
+        m1.setBounds(0, 0, 700, 500);
         m1.setVisible(true);
         m1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
