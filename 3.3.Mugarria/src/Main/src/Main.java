9a6add06598cec74d5c314d2ca7102b1d8f8e077
Igor Garcia <igordoble10@gmail.com>
2024-04-08 22:14:16 +0200

    Artxiboa sortu

---
+++ 3.3.Mugarria/src/Main/src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main extends JFrame {
+    private JComboBox<String> comboBox;
+    public static void main(String[] args) {
+    }
+}
